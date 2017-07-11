cdcd904599cb8577adc8574e3e676973f63dfc46
daifukunem <daifukunem@gmail.com>
2017-07-11 17:29:58 -0500

    Fix tip regex.

--- app.js
+++ app.js
@@ -217,7 +217,7 @@ function processTransaction(botWallet, tx) {
 }
 
 function monitorComments() {
-    let tipCommentStream = snooStream.commentStream(SUBREDDIT, { regex: /!tipxem\s+(\d+)/, rate: 30000 });
+    let tipCommentStream = snooStream.commentStream(SUBREDDIT, { regex: /!tipxem\s+((?=.*\d)\d*(?:\.\d+)?)/, rate: 30000 });
 
     tipCommentStream.on('post', (comment, match) => {
         var tipAmount = parseFloat(match[1]).toFixed(6);
